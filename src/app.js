332733a99ad2eaa5648635566c8e87e14dd4049c
willymiller41 <willymiller41@hotmamil.com>
2023-05-29 17:30:11 -0300

    subiendo ajustes en app, imagenes y hoja de estilos

--- src/app.js
+++ src/app.js
@@ -8,6 +8,26 @@ app.get("/", (req, res) =>{
     res.sendFile(path.join(__dirname, "../views/index.html"))
 });
 
+app.get("/login", (req, res) =>{
+    res.sendFile(path.join(__dirname, "../views/login.html"))
+});
+
+app.get("/register", (req, res) =>{
+    res.sendFile(path.join(__dirname, "../views/register.html"))
+});
+
+app.get("/productcart", (req, res) =>{
+    res.sendFile(path.join(__dirname, "../views/productCart.html"))
+});
+
+app.get("/nuestrahistoria", (req, res) =>{
+    res.sendFile(path.join(__dirname, "../views/nuestraHistoria.html"))
+});
+
+app.get("/productdetail", (req, res) =>{
+    res.sendFile(path.join(__dirname, "../views/productDetail.html"))
+});
+
 app.listen(3000, () =>{
     console.log("Servidor corriendo en el puerto 3000")
 })
